54758f7438c1e55e8b3d07cc3e8e7a01d8a108d1
Mitchell Grove <mgrove96@gmail.com>
2015-07-05 22:56:02 -0700

    fixed

--- Gemfile
+++ Gemfile
@@ -12,6 +12,7 @@ gem 'jbuilder', '~> 2.0'
 gem 'bootstrap-sass'
 gem 'devise', '~> 3.5.1'
 gem 'paperclip', '~> 4.2.3'
+gem 'aws-sdk', '< 2.0'
 
 group :production do
 	gem 'pg'
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,4 +80,13 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { :host => 'sunlitgrove.com' }
   # required by Devise for Heroku
   # note to set host name to actual
+
+  config.paperclip_defaults = {
+    :storage => :s3,
+    :s3_credentials => {
+      :bucket => ENV['AWS_BUCKET'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    }
+}
 end
